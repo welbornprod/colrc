c73fe0f6f798497836e4bd52982fcb791262be60
Christopher Welborn <cj@welbornprod.com>
2019-08-07 20:48:51 -0500

    cppcheck_run.sh has filter/exclude options.
    Instead of typing ` | grep ...`

--- tools/cppcheck_run.sh
+++ tools/cppcheck_run.sh
@@ -3,7 +3,7 @@
 # Runs cppcheck with some defaults.
 # -Christopher Welborn 07-27-2019
 appname="ColrC - cppcheck runner"
-appversion="0.0.1"
+appversion="0.1.0"
 apppath="$(readlink -f "${BASH_SOURCE[0]}")"
 appscript="${apppath##*/}"
 appdir="${apppath%/*}"
@@ -21,6 +21,7 @@ declare -a cppcheck_cmd=(
     "--enable=all"
     "--force"
     "--inline-suppr"
+    "--error-exitcode=1"
     # These are not needed at all:
     # "-I" "/usr/include"
     # "-I" "/usr/lib/gcc/x86_64-linux-gnu/7/include"
@@ -51,16 +52,23 @@ function print_usage {
 
     Usage:
         $appscript -h | -v
-        $appscript [-t] [-- ARGS...]
-        $appscript [FILE...] [-- ARGS...]
+        $appscript [-f pat] [-e pat] [-t] [-- ARGS...]
+        $appscript [-f pat] [-e pat] [FILE...] [-- ARGS...]
+        $appscript [-E] [-t] [-- ARGS...]
+        $appscript [-E] [FILE...] [-- ARGS...]
 
     Options:
-        ARGS          : Extra arguments for cppcheck.
-        FILE          : Zero or more files to check.
-                        If none are given, then the colr tool source is used.
-        -h,--help     : Show this message.
-        -t,--test     : Run on test files.
-        -v,--version  : Show $appname version and exit.
+        ARGS                  : Extra arguments for cppcheck.
+        FILE                  : Zero or more files to check.
+                                If none are given, the colr tool source is used.
+        -E,--errors           : Add (error)|(warn) to the --filter pattern.
+        -e pat,--exclude pat  : Exclude lines that match this pattern.
+                                These work in the order they are given.
+        -f pat,--filter pat   : Only show lines that match this pattern.
+                                These work in the order they are given.
+        -h,--help             : Show this message.
+        -t,--test             : Run on test files.
+        -v,--version          : Show $appname version and exit.
     "
 }
 
@@ -69,12 +77,31 @@ declare -a user_files
 declare -a extra_args
 
 in_args=0
+in_filter_arg=0
+in_exclude_arg=0
 do_test=0
+exclude_pat=""
+filter_pat=""
+do_errors=0
+filter_first=1
 
 for arg; do
     case "$arg" in
         "--")
             in_args=1
+            ;;
+        "-E" | "--errors")
+            do_errors=1
+            ;;
+        "-e" | "--exclude")
+            in_exclude_arg=1
+            in_filter_arg=0
+            [[ -z "$filter_pat" ]] && filter_first=0
+            ;;
+        "-f" | "--filter")
+            in_filter_arg=1
+            in_exclude_arg=0
+            [[ -z "$exclude_pat" ]] && filter_first=1
             ;;
         "-h" | "--help")
             print_usage ""
@@ -90,6 +117,12 @@ for arg; do
         -*)
             if ((in_args)); then
                 extra_args+=("$arg")
+            elif ((in_exclude_arg)); then
+                exclude_pat=$arg
+                in_exclude_arg=0
+            elif ((in_filter_arg)); then
+                filter_pat=$arg
+                in_filter_arg=0
             else
                 fail_usage "Unknown flag argument: $arg"
             fi
@@ -97,6 +130,12 @@ for arg; do
         *)
             if ((in_args)); then
                 extra_args+=("$arg")
+            elif ((in_exclude_arg)); then
+                exclude_pat=$arg
+                in_exclude_arg=0
+            elif ((in_filter_arg)); then
+                filter_pat=$arg
+                in_filter_arg=0
             else
                 user_files+=("$arg")
             fi
@@ -113,6 +152,36 @@ done
         user_files=("${colr_files[@]}" "${colr_tool_files[@]}")
     fi
 }
-"${cppcheck_cmd[@]}" "${extra_args[@]}" "${user_files[@]}"
-
+if ((do_errors)); then
+    if [[ -n "$filter_pat" ]]; then
+        if [[ "$filter_pat" == \(*\) ]]; then
+            filter_pat="$filter_pat|(error)|(warn)"
+        else
+            filter_pat="($filter_pat)|(error)|(warn)"
+        fi
+    else
+        filter_pat="(error)|(warn)"
+    fi
+fi
+if [[ -n "$exclude_pat" ]] && [[ -n "$filter_pat" ]]; then
+    if ((filter_first)); then
+        # shellcheck disable=SC1001
+        # ..i know shellcheck, it's a regular 'g'. I need the actual grep command.
+        "${cppcheck_cmd[@]}" "${extra_args[@]}" "${user_files[@]}" 2>&1 | \grep -E "$filter_pat" | \grep -E -v "$exclude_pat"
+    else
+        # shellcheck disable=SC1001
+        # ..i know shellcheck, it's a regular 'g'. I need the actual grep command.
+        "${cppcheck_cmd[@]}" "${extra_args[@]}" "${user_files[@]}" 2>&1 | \grep -E -v "$exclude_pat" | \grep -E "$filter_pat"
+    fi
+elif [[ -n "$exclude_pat" ]]; then
+    # shellcheck disable=SC1001
+    # ..i know shellcheck, it's a regular 'g'. I need the actual grep command.
+    "${cppcheck_cmd[@]}" "${extra_args[@]}" "${user_files[@]}" 2>&1 | \grep -E -v "$exclude_pat"
+elif [[ -n "$filter_pat" ]]; then
+    # shellcheck disable=SC1001
+    # ..i know shellcheck, it's a regular 'g'. I need the actual grep command.
+    "${cppcheck_cmd[@]}" "${extra_args[@]}" "${user_files[@]}" 2>&1 | \grep -E "$filter_pat"
+else
+    "${cppcheck_cmd[@]}" "${extra_args[@]}" "${user_files[@]}" 2>&1
+fi
 exit
